f2a964b17e53ec7d1c1fe06c902a8a0a3355d3bf
flamingCactus1 <dkykosh@novomind.com>
2025-10-10 15:40:17 +0200

    State of a program at the end of a 37th lecture

--- src/Hello.java
+++ src/Hello.java
@@ -1,20 +1,24 @@
 public class Hello {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello, Tim!");
 
         boolean isAlien = false;
-        if(!isAlien){
+        if (!isAlien) {
             System.out.println("It is not an alien");
             System.out.println("And i'm scared of aliens");
         }
         int topScore = 80;
-        if(topScore < 100){
+        if (topScore < 100) {
             System.out.println("You got the high score!");
         }
 
-        int secondTopScore = 81;
-        if(topScore >= secondTopScore && topScore < 100){
+        int secondTopScore = 80;
+        if ((topScore >= secondTopScore) && (topScore < 100)) {
             System.out.println("Greater than second top score & less than top score!");
+        }
+
+        if ((topScore > 90) || (secondTopScore < 90)) {
+            System.out.println("Either or both of the conditions are true");
         }
     }
 }
