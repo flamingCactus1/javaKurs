59d794c88af3f2ef0b1c33b443b98b6a7ff0cbb7
flamingCactus1 <dkykosh@novomind.com>
2025-10-10 13:48:12 +0200

    State of a program at the end of a 34th lecture

--- src/FirsClass.java
+++ src/FirsClass.java
@@ -2,9 +2,7 @@
 
 
 public class FirsClass {
-
-   public static void main(String[] args) {
-        System.out.println("Hello World");
+     public static void main(String[] args) {
+        System.out.println("Hello Tim");
     }
-
 }
