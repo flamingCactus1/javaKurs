1ff8d2dc9c5d39276ae92ce515a515904718d292
flamingCactus1 <dkykosh@novomind.com>
2025-10-10 15:27:52 +0200

    State of a program at the end of a 36th lecture

--- src/Hello.java
+++ src/Hello.java
@@ -7,5 +7,14 @@ public class Hello {
             System.out.println("It is not an alien");
             System.out.println("And i'm scared of aliens");
         }
+        int topScore = 80;
+        if(topScore < 100){
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if(topScore >= secondTopScore && topScore < 100){
+            System.out.println("Greater than second top score & less than top score!");
+        }
     }
 }
