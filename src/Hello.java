dea6f6aef18de4d4b1c23263db15084b6289d700
flamingCactus1 <dkykosh@novomind.com>
2025-10-10 13:54:48 +0200

    State of a program at the end of a 35th lecture

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,11 @@
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Hello World!");
         System.out.println("Hello, Tim!");
+
+        boolean isAlien = false;
+        if(!isAlien){
+            System.out.println("It is not an alien");
+            System.out.println("And i'm scared of aliens");
+        }
     }
 }
