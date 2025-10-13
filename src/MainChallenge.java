b4ded6fe5fa9b36d094023934f21a1771c80f187
flamingCactus1 <dkykosh@novomind.com>
2025-10-13 11:21:39 +0200

    State of a program at the end of a 49th lecture

--- src/MainChallenge.java
+++ src/MainChallenge.java
@@ -1,27 +1,32 @@
 public class MainChallenge {
 
     public static void main(String[] args) {
+        int[] scores = {1500, 1000, 500, 100, 25};
+        String[] names = {"Alex", "Dany", "Mindy", "Alex", "Disco"};
 
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-
-        System.out.println("Your final score is " + finalScoreCalculation(gameOver, score, levelCompleted, bonus));
-
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-        System.out.println("Your final score is " + finalScoreCalculation(gameOver, score, levelCompleted, bonus));
-
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(displayHighScorePosition(names[i], scores[i]));
+        }
     }
 
-    public static int finalScoreCalculation(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateHighScorePosition(int score) {
 
-        int result = gameOver ? (score + (levelCompleted * bonus) + 1000) : null;
-
-        return result;
-
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500) {
+            return 2;
+        } else if (score >= 100) {
+            return 3;
+        }
+        return 4;
     }
 
+    public static String displayHighScorePosition(String name, int score) {
+        if (calculateHighScorePosition(score) != 4) {
+            return (name + " managed to get to the " + calculateHighScorePosition(score) + " position on a high score list.");
+        } else {
+            return "Srry, bud, next time";
+        }
+
+    }
 }
