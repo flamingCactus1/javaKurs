2cde707ce9980e0e831daa132e4e0b3a15f03f4c
flamingCactus1 <dkykosh@novomind.com>
2025-10-13 14:22:42 +0200

    State of a program at the end of a 53nd lecture

---
+++ src/OverloadedMethodChallenge.java
@@ -0,0 +1,14 @@
+public class OverloadedMethodChallenge {
+    public static void main(String[] args) {
+        System.out.println(convertToCentimeters(2));
+        System.out.println(convertToCentimeters(3, 11));
+    }
+
+    public static double convertToCentimeters(int valueInInches){
+        return valueInInches * 2.54d;
+    }
+
+    public static double convertToCentimeters(int heightInFeet, int heightInInches){
+        return convertToCentimeters(heightInInches+(heightInFeet*12));
+    }
+}
