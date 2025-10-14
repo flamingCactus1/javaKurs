ccb4225984710d8e4d4f470e6662b44d54bed9bc
flamingCactus1 <dkykosh@novomind.com>
2025-10-14 14:49:25 +0200

    State of a program at the end of a 66th lecture

---
+++ src/WhileLoopChallenge.java
@@ -0,0 +1,18 @@
+public class WhileLoopChallenge {
+    public static void main(String[] args) {
+        int i = 5, counterOfEvenNumbers = 0;
+        while (i <= 20 &&  counterOfEvenNumbers < 5) {
+            if (isEvenNumber(i)) {
+                System.out.println(i + " is an even number");
+                counterOfEvenNumbers++;
+            }
+            i++;
+        }
+        System.out.println("The amount of even numbers is " + counterOfEvenNumbers);
+        System.out.println("The number of odd numbers is " + (15 - counterOfEvenNumbers));
+    }
+
+    public static boolean isEvenNumber(int number) {
+        return number % 2 == 0;
+    }
+}
