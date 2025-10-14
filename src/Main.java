3e0391ab1f202d6fd33ef712de39d86ecc6a4511
flamingCactus1 <dkykosh@novomind.com>
2025-10-14 08:29:49 +0200

    State of a program at the end of a 57th lecture

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        int value = (int) Math.floor(Math.random()*10);
+        switch (value) {
+            case 1:
+                System.out.println("value is 1");
+                break;
+            case 2:
+                System.out.println("value is 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("value is 3, 4, or 5");
+                System.out.println("value actually is " + value);
+                break;
+            default:
+                System.out.println("value is neither 1 or 2");
+                System.out.println(value);
 
+        }
     }
 }
