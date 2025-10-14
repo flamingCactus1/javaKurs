26c156b05483d94b059a9dc00e62aae43cf3a117
flamingCactus1 <dkykosh@novomind.com>
2025-10-14 14:33:18 +0200

    State of a program at the end of a 65th lecture

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        int value = (int) Math.floor(Math.random() * 10);
-        switch (value) {
-            case 1 -> System.out.println("value is 1");
-            case 2 -> System.out.println("value is 2");
-            case 3, 4, 5 -> {
-                System.out.println("value is 3, 4, or 5");
-                System.out.println("value actually is " + value);
-            }
-            default -> {
-                System.out.println("value is neither 1 or 2");
-                System.out.println(value);
-            }
-        }
-        String month = "Yo mama";
-        System.out.println(month + " is a " + getSeason(month) + " month");
-    }
 
-    public static String getSeason(String month) {
-        return switch (month) {
-            case "January", "February", "December" -> "Winter";
-            case "March", "April", "May" -> "Spring";
-            case "June", "July", "August" -> "Summer";
-            case "September", "October", "November" -> "Fall";
-            default -> month + " is a bad data";
-        };
+//        for (int i = 1; i <= 5; i++)
+//            System.out.println(i);
+//
+//        int j = 1;
+//        boolean isReady = false;
+//        do {
+//            j++;
+//            System.out.println(j);
+//        }while (isReady);
+        int number = 0;
+        while (number < 50) {
+            number = number + 5;
+            if(number % 25 == 0) continue;
+            System.out.println(number);
+        }
     }
 }
