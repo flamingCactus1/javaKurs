f83118a171abbd56bd66abd507a608d3d1e67643
flamingCactus1 <dkykosh@novomind.com>
2025-10-15 15:18:11 +0200

    State of a program at the end of a 72nd lecture

--- src/ReadingUserInput/Main.java
+++ src/ReadingUserInput/Main.java
@@ -1,11 +1,17 @@
 package ReadingUserInput;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int currentYear = 2025;
 
-        System.out.println(getInputFromConsole(currentYear));
-        System.out.println(getInputFromScanner(currentYear));
+        try {
+            System.out.println(getInputFromConsole(currentYear));
+        } catch (NullPointerException e) {
+            System.out.println(getInputFromScanner(currentYear));
+        }
     }
 
     public static String getInputFromConsole(int currentYear) {
@@ -20,7 +26,35 @@ public class Main {
     }
 
     public static String getInputFromScanner(int currentYear) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Hi, " + name + ", Thank you!");
 
-        return  "";
+        boolean validDateOfBirth = false;
+        int age = 0;
+        do {
+            System.out.println("Enter your year of birth: ");
+            String dateOfBirth = input.nextLine();
+            try {
+                age = checkData(currentYear, dateOfBirth);
+                validDateOfBirth = age != -1;
+            } catch (NumberFormatException e) {
+                System.out.println("characters are not allowed");
+            }
+            if (!validDateOfBirth) {
+                System.out.println("That is some serious BS");
+            }
+        } while (!validDateOfBirth);
+        return "So you are " + age + " years old";
+    }
+
+    public static int checkData(int currentYear, String dateOfBirth) {
+        int dob = Integer.parseInt(dateOfBirth);
+        int minimumYear = currentYear - 125;
+        if (dob < minimumYear || dob > currentYear) {
+            return -1;
+        }
+        return currentYear - dob;
     }
 }
