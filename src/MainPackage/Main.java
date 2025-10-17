9647c82f41e77a2e3cb695704e6e7ea63114cd6d
flamingCactus1 <dkykosh@novomind.com>
2025-10-17 11:40:50 +0200

    Lecture 88 is completed.

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -3,7 +3,7 @@ package MainPackage;
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            Student s = new Student("" + i,
+            LPAStudent s = new LPAStudent("" + i,
                     switch (i) {
                         case 1 -> "Mary";
                         case 2 -> "Carol";
@@ -15,7 +15,15 @@ public class Main {
                     "05/11/2000",
                     "Math");
             System.out.println(s.toString());
-            System.out.println(s);
         }
+        Student pojoStudent = new Student("s923006", "Ann", "05/11/1999", "Java Masterclass");
+        Student pojoStudent2 = pojoStudent;
+        LPAStudent recordStudent = new LPAStudent("s923007", "Bill", "05/11/1998", "Java Masterclass");
+        System.out.println(recordStudent);
+        System.out.println(pojoStudent);
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+
+        System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " + recordStudent.classList());
     }
 }
