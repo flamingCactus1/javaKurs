382f8dd253b1515497c1a46092118351a3550408
flamingCactus1 <dkykosh@novomind.com>
2025-10-17 08:23:13 +0200

    Lecture 87 is completed.

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -2,7 +2,20 @@ package MainPackage;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        car.describeCar();
+        for (int i = 0; i < 5; i++) {
+            Student s = new Student("" + i,
+                    switch (i) {
+                        case 1 -> "Mary";
+                        case 2 -> "Carol";
+                        case 3 -> "Tim";
+                        case 4 -> "Harry";
+                        case 5 -> "Lisa";
+                        default -> "Anonymous";
+                    },
+                    "05/11/2000",
+                    "Math");
+            System.out.println(s.toString());
+            System.out.println(s);
+        }
     }
 }
