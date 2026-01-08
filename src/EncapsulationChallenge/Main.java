9dc3195fd890d84d6bba35dec6f516997bf4d295
flamingCactus1 <dkykosh@novomind.com>
2026-01-08 12:12:03 +0100

    Encapsulation challenge is completed

---
+++ src/EncapsulationChallenge/Main.java
@@ -0,0 +1,20 @@
+package EncapsulationChallenge;
+
+public class Main {
+    public static void main(String[] args) {
+        Printer samsungPrinter = new Printer();
+        Printer xeroxPrinter = new Printer(true);
+        Printer brotherPrinter = new Printer(0, 2000, true);
+
+        samsungPrinter.addTonerLevel(10);
+        samsungPrinter.addTonerLevel(-20);
+        samsungPrinter.printPages(20);
+
+        xeroxPrinter.printPages(20);
+        xeroxPrinter.printPages(-20);
+        System.out.println("Xerox printer has printed " + xeroxPrinter.getPagesPrinted() + " pages");
+
+
+        brotherPrinter.printPages(20);
+    }
+}
---
+++ src/EncapsulationChallenge/Printer.java
@@ -0,0 +1,58 @@
+package EncapsulationChallenge;
+
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, int pagesPrinted, boolean duplex) {
+        this.tonerLevel = tonerLevel > 100 ? 100 : tonerLevel;
+        if (tonerLevel < 0) {
+            this.tonerLevel = 0;
+        }
+        this.pagesPrinted = pagesPrinted < 0 ? 0 : pagesPrinted;
+        this.duplex = duplex;
+    }
+
+    public Printer() {
+        this(100, 0, false);
+    }
+
+    public Printer (boolean duplex) {
+        this(100, 0, duplex);
+    }
+
+    public int getTonerLevel() {
+        return this.tonerLevel;
+    }
+
+    public int getPagesPrinted() {
+        return this.pagesPrinted;
+    }
+
+    public boolean isDuplex() {
+        return this.duplex;
+    }
+
+    public void addTonerLevel(int tonerAmount) {
+        if(tonerAmount < 0) {
+            System.out.println("You can not add a negative amount of toner");
+            return;
+        }
+        this.tonerLevel = (tonerAmount + this.tonerLevel) > 100 ? 100 : this.tonerLevel +  tonerAmount;
+        System.out.println("Toner Level: " + this.tonerLevel);
+    }
+
+    public void printPages(int pagesToPrint) {
+        if (this.isDuplex()) {
+            pagesToPrint /= 2;
+        }
+        if (pagesToPrint <= 0 || this.tonerLevel <= 0) {
+            System.out.println("No pages will be printed");
+        } else {
+            System.out.println(pagesToPrint + " pages will be printed");
+            this.pagesPrinted += pagesToPrint;
+            this.tonerLevel -= 1;
+        }
+    }
+}
